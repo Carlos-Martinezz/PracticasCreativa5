f3b29357c732a9e1515ab89f0e53dc384102172c
Carlos Martínez <carlos33820@gmail.com>
2020-12-08 15:46:04 -0600

    Update Secuencia.java

--- src/com/secuencia/main/Secuencia.java
+++ src/com/secuencia/main/Secuencia.java
@@ -1,7 +1,7 @@
 package com.secuencia.main;
 
 /**
- * @author Carlos MartÌnez
+ * @author Carlos Mart√≠nez
  *
  */
 public class Secuencia {
@@ -9,12 +9,12 @@ public class Secuencia {
 	public static void main(String[] args) {
 		
 		/*Instanciamos el objeto y le indicamos una longitud
-		 * En nuestro caso ser· hasta el 47 para mostrar el n˙mero que se imprimir· en esa posiciÛn
+		 * En nuestro caso ser√° hasta el 47 para mostrar el n√∫mero que se imprimir√° en esa posici√≥n
 		 * luego llamaos el metodo mostrarSecuencia
 		 * 
-		 * PosiciÛn 47: 1836311903
+		 * Posici√≥n 47: 1836311903
 		 * */
-		Fibonacci f1 = new Fibonacci("fibonacci 1", 47); 
+		Fibonacci f1 = new Fibonacci("Fibonacci 1", 47); 
 		f1.mostrarSecuencia(); 
 		
 	}
